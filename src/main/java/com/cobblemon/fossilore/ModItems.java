167d0f3eb43e38a26afbce01f57ceef1570d44d3
Fagghino <fgiordano839@gmail.com>
2026-05-19 12:55:56 +0200

    refactor: update ModItems.java

--- src/main/java/com/cobblemon/fossilore/ModItems.java
+++
@@ -1,7 +0,0 @@
-package com.cobblemon.fossilore;
-
-public class ModItems {
-    public static void register() {
-        CobblemonFossilOre.LOGGER.info("Registering items for " + CobblemonFossilOre.MOD_ID);
-    }
-}
