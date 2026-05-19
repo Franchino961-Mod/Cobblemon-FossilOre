cc7e164c2c6ffdcdf0f756d1303af0c60b3b5362
Fagghino <fgiordano839@gmail.com>
2026-05-19 12:56:02 +0200

    refactor: update ModOreGeneration package and class references

--- src/main/java/com/fossilore/world/ModOreGeneration.java
+++ src/main/java/com/fossilore/world/ModOreGeneration.java
@@ -1,7 +1,6 @@
-package com.cobblemon.fossilore.world;
+package com.fossilore.world;
 
-import com.cobblemon.fossilore.CobblemonFossilOre;
-import com.cobblemon.fossilore.ModBlocks;
+import com.fossilore.FossilOre;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.registry.RegistryKey;
@@ -10,45 +9,43 @@ import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.Registries;
 
 public class ModOreGeneration {
 
     // New thematic ores
     public static final RegistryKey<net.minecraft.world.gen.feature.ConfiguredFeature<?, ?>> MARINE_FOSSIL_ORE_CONFIGURED = 
         RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, 
-            Identifier.of(CobblemonFossilOre.MOD_ID, "marine_fossil_ore"));
+            Identifier.of(FossilOre.MOD_ID, "marine_fossil_ore"));
     
     public static final RegistryKey<net.minecraft.world.gen.feature.PlacedFeature> MARINE_FOSSIL_ORE_PLACED = 
         RegistryKey.of(RegistryKeys.PLACED_FEATURE, 
-            Identifier.of(CobblemonFossilOre.MOD_ID, "marine_fossil_ore"));
+            Identifier.of(FossilOre.MOD_ID, "marine_fossil_ore"));
 
     public static final RegistryKey<net.minecraft.world.gen.feature.ConfiguredFeature<?, ?>> TERRESTRIAL_FOSSIL_ORE_CONFIGURED = 
         RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, 
-            Identifier.of(CobblemonFossilOre.MOD_ID, "terrestrial_fossil_ore"));
+            Identifier.of(FossilOre.MOD_ID, "terrestrial_fossil_ore"));
     
     public static final RegistryKey<net.minecraft.world.gen.feature.PlacedFeature> TERRESTRIAL_FOSSIL_ORE_PLACED = 
         RegistryKey.of(RegistryKeys.PLACED_FEATURE, 
-            Identifier.of(CobblemonFossilOre.MOD_ID, "terrestrial_fossil_ore"));
+            Identifier.of(FossilOre.MOD_ID, "terrestrial_fossil_ore"));
 
     public static final RegistryKey<net.minecraft.world.gen.feature.ConfiguredFeature<?, ?>> ANCIENT_FOSSIL_ORE_CONFIGURED = 
         RegistryKey.of(RegistryKeys.CONFIGURED_FEATURE, 
-            Identifier.of(CobblemonFossilOre.MOD_ID, "ancient_fossil_ore"));
+            Identifier.of(FossilOre.MOD_ID, "ancient_fossil_ore"));
     
     public static final RegistryKey<net.minecraft.world.gen.feature.PlacedFeature> ANCIENT_FOSSIL_ORE_PLACED = 
         RegistryKey.of(RegistryKeys.PLACED_FEATURE, 
-            Identifier.of(CobblemonFossilOre.MOD_ID, "ancient_fossil_ore"));
+            Identifier.of(FossilOre.MOD_ID, "ancient_fossil_ore"));
 
     // Biome tags
     public static final TagKey<Biome> HAS_MARINE_FOSSIL_ORE = 
-        TagKey.of(RegistryKeys.BIOME, Identifier.of(CobblemonFossilOre.MOD_ID, "has_marine_fossil_ore"));
+        TagKey.of(RegistryKeys.BIOME, Identifier.of(FossilOre.MOD_ID, "has_marine_fossil_ore"));
     
     public static final TagKey<Biome> HAS_ANCIENT_FOSSIL_ORE = 
-        TagKey.of(RegistryKeys.BIOME, Identifier.of(CobblemonFossilOre.MOD_ID, "has_ancient_fossil_ore"));
+        TagKey.of(RegistryKeys.BIOME, Identifier.of(FossilOre.MOD_ID, "has_ancient_fossil_ore"));
 
     public static void register() {
-        CobblemonFossilOre.LOGGER.info("Registering ore generation for " + CobblemonFossilOre.MOD_ID);
+        FossilOre.LOGGER.info("Registering ore generation for " + FossilOre.MOD_ID);
         
         // Marine Fossil Ore - aquatic biomes (ocean, river, beach, swamp)
         BiomeModifications.addFeature(
